c3414671d2cd6e803ec890a2a834b8e74911ed44
Tyson <60005863@ironschools.org>
2018-09-19 10:12:11 -0600

    created this file

---
+++ Chapter 1/Ex_1_5.java
@@ -0,0 +1,12 @@
+/*
+Author: Tyson Imlay
+Date: 9/19/18
+
+Output result of equatn
+*/
+
+class Ex_1_5 {
+	public static void main(String[] args) {
+		System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+	}
+}
